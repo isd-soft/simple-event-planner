4bdab39acfd991f48e574a1f391d5fe9b8a3655c
Vlad Diorditsa <vladislav.diorditsa@gmail.com>
2021-07-05 15:34:09 +0300

    Updated user

--- src/main/java/com/internship/sep/models/User.java
+++ src/main/java/com/internship/sep/models/User.java
@@ -1,32 +1,31 @@
 package com.internship.sep.models;
 
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@EqualsAndHashCode
-public class User {
+import javax.persistence.*;
 
+@Entity
+@Data
+@Table(name = "User")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String email;
+
     private String firstName;
+
     private String lastName;
+
     private Integer age;
+
     private String phoneNumber;
+
     private String password;
+
     private Role role;
 
-    public User() {}
-
-    public User(String email, String firstName, String lastName, Integer age, String phoneNumber, String password, Role role) {
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.phoneNumber = phoneNumber;
-        this.password = password;
-        this.role = role;
-    }
 }
