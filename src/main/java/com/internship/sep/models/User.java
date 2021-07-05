20bc8a1b20123a650e63df0a0a3e503f0d7a58d3
Vlad Diorditsa <vladislav.diorditsa@gmail.com>
2021-07-05 16:33:25 +0300

    Added toString, equals, hashCode to User

--- src/main/java/com/internship/sep/models/User.java
+++ src/main/java/com/internship/sep/models/User.java
@@ -1,11 +1,13 @@
 package com.internship.sep.models;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "User")
 public class User {
     @Id
@@ -13,15 +15,42 @@ public class User {
     private Long id;
     private String email;
     private String firstName;
-
     private String lastName;
-
     private Integer age;
-
     private String phoneNumber;
-
     private String password;
-
+    @Enumerated(EnumType.STRING)
     private Role role;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if(!(o instanceof User))
+            return false;
+
+        User other = (User) o;
+        return id != null &&
+                id.equals(other.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
+
