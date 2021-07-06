4ab6bac930c88f9415a684d637af7fb50e04ebc9
Andrei Padureanu <andrei.padureanu67@gmail.com>
2021-07-06 14:27:11 +0300

    Added some SQL constraints to Table fields

--- src/main/java/com/internship/sep/models/User.java
+++ src/main/java/com/internship/sep/models/User.java
@@ -13,11 +13,24 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(
+            nullable = false,
+            unique = true
+    )
     private String email;
+
+    @Column(nullable = false)
     private String firstName;
     private String lastName;
+
+    @Column(nullable = false)
     private Integer age;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
+    @Column(nullable = false)
     private String password;
 
     @Enumerated(value = EnumType.STRING)
