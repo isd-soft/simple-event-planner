5af71f52bd282a504444f0eaa945d243f99c1abf
mr-denis <denis_lupashko@live.com>
2021-07-06 10:47:09 +0300

    fix

---
+++ src/main/java/com/internship/sep/SepApplication.java
@@ -0,0 +1,13 @@
+package com.internship.sep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepApplication.class, args);
+	}
+
+}
