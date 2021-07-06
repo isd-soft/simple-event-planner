e45a3d5f1eca7f5cdb1b237740c336e658f8a160
Vlad Diorditsa <vladislav.diorditsa@gmail.com>
2021-07-06 13:15:12 +0300

    Added entity to dto mapping, Added data annotation to UserDTO

--- src/main/java/com/internship/sep/web/EventDTO.java
+++ src/main/java/com/internship/sep/web/EventDTO.java
@@ -1,6 +1,7 @@
 package com.internship.sep.web;
 
 import com.internship.sep.models.Attendee;
+import com.internship.sep.models.Event;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -19,5 +20,16 @@ public class EventDTO {
     private Boolean isApproved;
 
     private List <Attendee> attendees = new ArrayList<>();
+
+    public EventDTO(Event entity) {
+        this.id = entity.getId();
+        this.attendees = entity.getAttendees();
+        this.category = entity.getCategory();
+        this.description = entity.getDescription();
+        this.startDateTime = entity.getStartDateTime();
+        this.endDateTime = entity.getEndDateTime();
+        this.isApproved = entity.getIsApproved();
+        this.name = entity.getName();
+    }
 
 }
--- src/main/java/com/internship/sep/web/UserDTO.java
+++ src/main/java/com/internship/sep/web/UserDTO.java
@@ -1,5 +1,8 @@
 package com.internship.sep.web;
+import com.internship.sep.models.User;
+import lombok.Data;
 
+@Data
 public class UserDTO {
     private Long id;
     private String email;
@@ -8,4 +11,14 @@ public class UserDTO {
     private Integer age;
     private String phoneNumber;
     private String password;
+
+    public UserDTO(User entity) {
+        this.id = entity.getId();
+        this.email = entity.getEmail();
+        this.firstName = entity.getFirstName();
+        this.lastName = entity.getLastName();
+        this.age = entity.getAge();
+        this.phoneNumber = entity.getPhoneNumber();
+        this.password = entity.getPassword();
+    }
 }
