75bb39fb9800ae6ebfad555d49000bd40883b69d
mr-denis <denis_lupashko@live.com>
2021-07-07 18:17:07 +0300

    event controller INTERMEDIATE CHANGES

--- src/main/java/com/internship/sep/web/controllers/EventController.java
+++ src/main/java/com/internship/sep/web/controllers/EventController.java
@@ -3,6 +3,8 @@ package com.internship.sep.web.controllers;
 import com.internship.sep.services.EventService;
 import com.internship.sep.web.EventDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,27 +19,32 @@ public class EventController {
     private final EventService eventService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<EventDTO> getEvents() {
         return eventService.getAllEvents();
     }
 
-    @GetMapping(path = "{eventId}")
+    @GetMapping(path = "/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
     public EventDTO getEvent(@PathVariable("eventId") Long eventId) {
         return eventService.getEventById(eventId);
     }
 
     @PostMapping
-    public void addEvent(@RequestBody EventDTO eventDTO) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createEvent(@RequestBody EventDTO eventDTO) {
         eventService.createNewEvent(eventDTO);
     }
 
-    @PutMapping(path = "{eventId}")
-    public void changeEvent(@PathVariable("eventId") Long eventId,
+    @PutMapping(path = "/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateEvent(@PathVariable("eventId") Long eventId,
                             @RequestBody EventDTO eventDTO) {
         eventService.saveEventByDTO(eventDTO);
     }
 
     @PutMapping(path = "/accept/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
     public void acceptEvent(@PathVariable("eventId") Long eventId) {
         // TODO: Cale doar pentru admini
         // TODO: eventService.acceptEvent();
@@ -45,7 +52,15 @@ public class EventController {
     }
 
     @DeleteMapping(path = "{eventId}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteEvent(@PathVariable("eventId") Long eventId) {
         eventService.deleteEventById(eventId);
+    }
+
+    @PatchMapping(path = "/{eventId}")
+    public ResponseEntity<EventDTO> patchEvent(@PathVariable("eventId") Long eventId,
+                                              @RequestBody EventDTO eventDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(eventService.patchEvent(eventDTO));
+        //return eventService.patchEvent(eventDTO);
     }
 }
