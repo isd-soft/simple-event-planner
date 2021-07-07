10dda8fdd9e20f12eb369eb5bb5720e85f45e8b2
Andrei Padureanu <andrei.padureanu67@gmail.com>
2021-07-07 16:04:27 +0300

    Added AuthController

--- src/main/java/com/internship/sep/web/controllers/AuthController.java
+++ src/main/java/com/internship/sep/web/controllers/AuthController.java
@@ -1,24 +1,61 @@
 package com.internship.sep.web.controllers;
 
+import com.internship.sep.security.jwt.JwtRequest;
+import com.internship.sep.security.jwt.JwtResponse;
+import com.internship.sep.security.jwt.JwtTokenUtil;
 import com.internship.sep.services.UserService;
 import com.internship.sep.web.UserDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(path = "/auth")
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserDetailsService userDetailsService;
+
     private final UserService userService;
 
-    @PostMapping(path = "/register")
-    public void registerUser(@RequestBody UserDTO user) {
-        userService.addUser(user);
-        // TODO: Obtinerea acces [JWT]
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody JwtRequest request) throws Exception {
+        System.out.println("Request = " + request);
+        authenticate(request.getEmail(), request.getPassword());
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
-    @PostMapping(path = "/login")
-    public void login(@RequestParam String email, @RequestParam String password) {
-        // TODO: Obtinerea acces [JWT]
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserDTO userDTO) throws Exception {
+        System.out.println(userDTO);
+        userService.addUser(userDTO);
+        authenticate(userDTO.getEmail(), userDTO.getPassword());
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userDTO.getEmail());
+
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
     }
+
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("User Disabled", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("Invalid Credentials", e);
+        }
+    }
+
 }
