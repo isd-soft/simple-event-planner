52e14876d801d06141143c98020985e757f3d4f0
Andrei Padureanu <andrei.padureanu67@gmail.com>
2021-07-13 12:22:34 +0300

    Added Token Interceptor angular

---
+++ frontend/src/app/urls.config.ts
@@ -0,0 +1,6 @@
+export const BASE_URL: string = "http://localhost:8080";
+export const SIGN_IN: string = "/login";
+export const SIGN_IN_URL: string = BASE_URL + SIGN_IN;
+
+export const SIGN_UP: string = "/register";
+export const SING_UP_URL: string = BASE_URL + SIGN_UP;
